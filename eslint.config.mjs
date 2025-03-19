f7f02031ee17ecf6fad77ad3b34ff6deac443449
风风 <qwe18173945756@163.com>
2025-03-19 19:19:51 +0800

    test lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      indent: ["error", 2], // 缩进 2 个空格
+      semi: ["error", "always"], // 强制分号
+      "no-console": ["error"], // 允许 .js 文件中使用 console
+      "no-require": "off", 
+    },
+  },
+];
--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
 function $(el) {
-    const elArr = document.querySelectorAll(el)
-    if (elArr.length > 1) {
-        return Array.from(elArr)
-    } else {
-        return elArr[0]
-    }
+  const elArr = document.querySelectorAll(el);
+  if (elArr.length > 1) {
+    return Array.from(elArr);
+  } else {
+    return elArr[0];
+  }
 }
 
 (() => {
-    let h1 = $('h1');
-    let btn = $("button")
-    btn[0].onclick = () => {
-        h1.style.color = "blue"
-    }
-    btn[1].onclick = () => {
-        h1.style.color = "black"
-    }
-    console.log(import.meta.env.VITE_GLOB_API_URL)
-})()
+  let h1 = $('h1');
+  let btn = $("button");
+  btn[0].onclick = () => {
+    h1.style.color = "blue";
+  };
+  btn[1].onclick = () => {
+    h1.style.color = "black";
+  };
+  console.log(import.meta.env.VITE_GLOB_API_URL);
+})();
 
